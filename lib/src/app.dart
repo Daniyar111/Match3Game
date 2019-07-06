373c80b9c9ef00a90751876ad2d8de72b2ff13a1
Daniyar111 <dani.changylov@gmail.com>
2019-07-06 06:57:22 -0700

    init application

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
+import 'pages/home_page.dart';
+
 class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
 
-    return Container();
+    return MaterialApp(
+      title: 'Crush Game',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: HomePage(),
+    );
   }
 }
