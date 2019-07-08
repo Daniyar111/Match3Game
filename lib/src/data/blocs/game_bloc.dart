75040f668929614c04f72fc46b5471cd5210b7fa
Daniyar111 <dani.changylov@gmail.com>
2019-07-08 02:57:57 -0700

    GameBloc -> streams are written, need to finish setLevel, loadLevel

--- lib/src/data/blocs/game_bloc.dart
+++ lib/src/data/blocs/game_bloc.dart
@@ -83,6 +83,7 @@ class GameBloc extends BlocBase{
 
   _loadLevels() async {
 
+
   }
 
 
---
+++ lib/src/data/constants.dart
@@ -0,0 +1,6 @@
+class Constants{
+
+  // Max number of tiles per row (and per column)
+  static double kMaxTilesPerRowAndColumn = 12.0;
+  static double kMaxTilesSize = 28.0;
+}
